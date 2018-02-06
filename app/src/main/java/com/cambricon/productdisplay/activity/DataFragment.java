639842ebd03647d6577e57aa9b191d055db05ed4
huangyaling <952287954@qq.com>
2018-02-06 16:05:08 +0800

    data count

--- app/src/main/java/com/cambricon/productdisplay/activity/DataFragment.java
+++ app/src/main/java/com/cambricon/productdisplay/activity/DataFragment.java
@@ -1,25 +1,54 @@
 package com.cambricon.productdisplay.activity;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
+import android.widget.LinearLayout;
 
 import com.cambricon.productdisplay.R;
-import com.cambricon.productdisplay.adapter.GridViewAdapter;
+import com.cambricon.productdisplay.adapter.TabAdapter;
 
 /**
  * Created by dell on 18-2-3.
  */
 
 public class DataFragment extends Fragment{
+    private ViewPager viewPager;
+    private View view;
+    private TabAdapter tabAdapter;
+    private TabLayout tabLayout;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.data_fragment, null);
+        view = inflater.inflate(R.layout.data_fragment, null);
+        initView();
         return view;
+    }
+
+    public void initView(){
+        viewPager=view.findViewById(R.id.data_count_viewpager);
+        tabLayout=view.findViewById(R.id.tab_layout);
+        tabAdapter=new TabAdapter(getActivity().getSupportFragmentManager(),getContext());
+        viewPager.setAdapter(tabAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        //设置分割线
+        LinearLayout linear = (LinearLayout)tabLayout.getChildAt(0);
+        linear.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+        linear.setDividerDrawable(ContextCompat.getDrawable(getContext(),R.drawable.divider));
+        //设置分割线间隔
+        linear.setDividerPadding(dip2px(15));
+    }
+
+    //像素单位转换
+    public int dip2px(int dip) {
+        float density = getResources().getDisplayMetrics().density;
+        return (int) (dip * density + 0.5);
     }
 }
---
+++ app/src/main/java/com/cambricon/productdisplay/adapter/TabAdapter.java
@@ -0,0 +1,43 @@
+package com.cambricon.productdisplay.adapter;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.cambricon.productdisplay.R;
+import com.cambricon.productdisplay.activity.TabFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by dell on 18-2-6.
+ */
+
+public class TabAdapter extends FragmentPagerAdapter {
+    private List<Fragment> fragments = new ArrayList<>();
+    private List<String> tabs = new ArrayList<>();
+    public TabAdapter(FragmentManager fm,Context context) {
+        super(fm);
+        tabs.add(context.getString(R.string.gv_text_item1));
+        tabs.add(context.getString(R.string.gv_text_item2));
+        fragments.add(new TabFragment(context,tabs.get(0)));
+        fragments.add(new TabFragment(context,tabs.get(1)));
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs.get(position);
+    }
+}
