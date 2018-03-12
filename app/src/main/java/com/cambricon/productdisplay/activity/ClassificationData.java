54a24c8e3221cf6642fe7405f21842274da274ba
huangyaling <952287954@qq.com>
2018-03-12 14:38:00 +0800

    data modify

--- app/src/main/java/com/cambricon/productdisplay/activity/ClassificationData.java
+++ app/src/main/java/com/cambricon/productdisplay/activity/ClassificationData.java
@@ -51,19 +51,31 @@ public class ClassificationData extends Fragment {
     private String content;
     private LinearLayout linearLayout;
     private GraphicalView graphicalView;
+    //cpu param
     private int[] points;
     private double[] avgTimes;
     private static double avgTimeValue = 0.00;
     private static int avgFpsValue;
+    //ipu param
+    private int[] ipu_points;
+    private double[] avgIPUTimes;
+    private static double avgIPUTimeValue = 0.00;
+    private static int avgIPUFpsValue;
+
     private TextView fps_tv;
     private TextView time_tv;
     private Button result_btn;
+    private TextView fps_ipu;
+    private TextView time_ipu;
+    private Button ipu_result;
+
     private ClassificationDB classificationDB;
     private UltraViewPager ultraViewPager_dialog;
+    private UltraViewPager ultraViewPager_ipuDialog;
     private PagerAdapter adapter_dialog;
     private ArrayList<ClassificationImage> allTicketsList;
     private ArrayList<ClassificationImage> allIPUTicketsList;
-    private int[] ipu_points;
+
     private int max = 0;
     private int min = 10000;
 
@@ -100,11 +112,17 @@ public class ClassificationData extends Fragment {
         fps_tv = view.findViewById(R.id.avg_fps);
         time_tv = view.findViewById(R.id.avg_time);
         result_btn = view.findViewById(R.id.all_result);
+        fps_ipu=view.findViewById(R.id.avg_ipu_fps);
+        time_ipu=view.findViewById(R.id.avg_ipu_time);
+        ipu_result=view.findViewById(R.id.all_ipu_result);
     }
 
     private void getData() {
         double allTime = 0.00;
         int allFps = 0;
+
+        double allIPUTime=0.00;
+        int allIPUFps=0;
         allTicketsList = new ArrayList<>();
         allTicketsList = classificationDB.fetchAll();
         points = new int[Config.ChartPointNum];
@@ -113,6 +131,7 @@ public class ClassificationData extends Fragment {
         allIPUTicketsList=classificationDB.fetchIPUAll();
         ipu_points=new int[Config.ChartPointNum];
         if(allIPUTicketsList.size()!=0){
+            avgIPUTimes = new double[allIPUTicketsList.size()];
             int ipuCount=0;
             if(Config.ChartPointNum>allIPUTicketsList.size()){
                 ipuCount=allIPUTicketsList.size();
@@ -122,7 +141,16 @@ public class ClassificationData extends Fragment {
 
             for(int j=0;j<ipuCount;j++){
                 ipu_points[j]=ConvertUtil.getFps(allIPUTicketsList.get(allIPUTicketsList.size()-j-1).getFps());
+                avgIPUTimes[j] = ConvertUtil.convert2Double(allTicketsList.get(allTicketsList.size()-j-1).getTime());
+                allIPUTime = allTime + avgIPUTimes[j];
+                allIPUFps = allFps + ipu_points[j];
             }
+            avgIPUTimeValue = allIPUTime / ipuCount;
+            avgIPUFpsValue = (int) allIPUFps / ipuCount;
+            fps_ipu.setText(R.string.classification_avg_time);
+            time_ipu.setText(R.string.classification_single_time);
+            fps_ipu.append(String.valueOf(avgIPUFpsValue) + "张/分钟");
+            time_ipu.append(String.valueOf((int) avgIPUTimeValue) + "ms");
         }
         if (allTicketsList.size() != 0) {
             avgTimes = new double[allTicketsList.size()];
@@ -147,8 +175,8 @@ public class ClassificationData extends Fragment {
             }
             avgTimeValue = allTime / dataSum;
             avgFpsValue = (int) allFps / dataSum;
-            fps_tv.setText("平均FPS值：");
-            time_tv.setText("单张图片平均时间：");
+            fps_tv.setText(R.string.classification_avg_time);
+            time_tv.setText(R.string.classification_single_time);
             fps_tv.append(String.valueOf(avgFpsValue) + "张/分钟");
             time_tv.append(String.valueOf((int) avgTimeValue) + "ms");
         }
@@ -177,6 +205,32 @@ public class ClassificationData extends Fragment {
                 } else {
                     Toast.makeText(getContext(), getString(R.string.classification_dialog_null), Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        ipu_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ResultDialog dialog = new ResultDialog(getContext());
+                View contentView2 = LayoutInflater.from(getContext()).inflate(
+                        R.layout.result_dialog, null);
+                ultraViewPager_ipuDialog = contentView2.findViewById(R.id.ultra_viewpager_dialog);
+                ultraViewPager_ipuDialog.setScrollMode(UltraViewPager.ScrollMode.HORIZONTAL);
+                adapter_dialog = new UltraPagerAdapter(true, allTicketsList);
+                ultraViewPager_ipuDialog.setAdapter(adapter_dialog);
+                ultraViewPager_ipuDialog.setMultiScreen(0.6f);
+                ultraViewPager_ipuDialog.setItemRatio(1.0f);
+                ultraViewPager_ipuDialog.setAutoMeasureHeight(true);
+                ultraViewPager_ipuDialog.setPageTransformer(false, new UltraDepthScaleTransformer());
+                dialog.setContentView(contentView2);
+                dialog.setTitle("测试结果");
+                dialog.setCanceledOnTouchOutside(true);
+                if (allIPUTicketsList.size() > 0) {
+                    dialog.show();
+                } else {
+                    Toast.makeText(getContext(), getString(R.string.classification_dialog_null), Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
@@ -254,7 +308,6 @@ public class ClassificationData extends Fragment {
             seriesRenderer.addXTextLabel(i, String.valueOf(i));
         }
         seriesRenderer.setPointSize(5);//设置坐标点大小
-
         seriesRenderer.setMarginsColor(Color.WHITE);//设置外边距空间的颜色
         seriesRenderer.setClickEnabled(false);
         //seriesRenderer.setChartTitle(getContext().getResources().getString(R.string.classification_chart_title));
@@ -271,7 +324,6 @@ public class ClassificationData extends Fragment {
         xySeriesRenderer1.setDisplayChartValues(true);//设置是否显示坐标点的y轴坐标值
         xySeriesRenderer1.setChartValuesTextSize(25);//设置显示的坐标点值的字体大小
         xySeriesRenderer1.setDisplayChartValuesDistance(30);
-
 
          /*某一组数据的描绘器，描绘该组数据的个性化显示效果，主要是字体跟颜色的效果*/
         XYSeriesRenderer xySeriesRenderer2 = new XYSeriesRenderer();
